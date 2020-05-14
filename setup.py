35cbd9c4d96750af47b4dcb912c2385e12200c1c
Michael Recachinas <mgr3yp@virginia.edu>
2020-05-14 20:58:02 +0100

    chore: run black and add MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
+from setuptools import setup
 
-from distutils.core import setup
 
-setup(name='clicktimepy',
-      version='2.0',
-      description='Python library that supports ClickTime REST v2',
-      author='Michael Ihde',
-      author_email='mihde@spectric.com',
-      url='https://github.com/spectriclabs/clicktimepy',
-      py_modules=['clicktime'],
-     )
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name="clicktimepy",
+    version="2.0",
+    description="Python library that supports ClickTime REST v2",
+    long_description=readme,
+    author="Michael Ihde",
+    author_email="mihde@spectric.com",
+    url="https://github.com/spectriclabs/clicktimepy",
+    py_modules=["clicktime"],
+)
