03e132785d8543198ba23df966f8deb69cb7dc9c
Michael Recachinas <mgr3yp@virginia.edu>
2020-05-14 20:58:14 +0100

    chore: run black on examples

--- examples/last_month_report.py
+++ examples/last_month_report.py
@@ -1,4 +1,4 @@
-#/usr/bin/env python
+# /usr/bin/env python
 import clicktime
 import datetime
 import calendar
@@ -10,12 +10,12 @@ logging.basicConfig(level=logging.DEBUG)
 with open(".token") as f:
     token = f.read().strip()
 
-# Connect to clicktime 
+# Connect to clicktime
 ct = clicktime.ClickTime(token=token)
 
 # Figure out the query date range
 today = datetime.date.today()
-prev_month = (today.month - 1)
+prev_month = today.month - 1
 prev_year = today.year
 if prev_month == 0:
     prev_month = 12
@@ -24,13 +24,9 @@ prev_last_day = calendar.monthrange(prev_year, prev_month)[1]
 start_date = datetime.date(year=prev_year, month=prev_month, day=1)
 end_date = datetime.date(year=prev_year, month=prev_month, day=prev_last_day)
 
-query = ct.reports(
-).resolve(
-    "JobID", "TaskID", "UserID"
-).params(
-    StartDate=start_date,
-    EndDate=end_date
-)
+query = ct.reports()
+    .resolve("JobID", "TaskID", "UserID")
+    .params(StartDate=start_date, EndDate=end_date)
 
 for doc in query.scroll():
     print(doc)
